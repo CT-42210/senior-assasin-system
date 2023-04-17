6a0c53ad0934014548f094087ad3393ced39c755
CT-42210 <nicholastroiano123@gmail.com>
2023-04-17 11:53:53 -0400

    fuck

--- main.py
+++ main.py
@@ -3,22 +3,22 @@
 username = input("USERNAME: \n -$")
 password = input("PASSWORD: \n -$")
 
-def merit_reader(discord_id):
-    d_id = str(discord_id)
 
-    with open("merit.txt", 'r') as f:
+def login():
+    with open("usernames.txt", 'r') as f:
         for number, line in enumerate(f):
-            if str(d_id) not in line:
-                merit_total = 0
-            if str(d_id) in line:
+            if str(username) not in line:
+                pass
+            if str(username) in line:
                 line_number = number
+                print(line_number)
 
-                with open("merit.txt", 'r') as f:
+                with open("passwords.txt", 'r') as f:
                     file_read = f.readlines()
-                    file_int1_read = int(line_number)
-                    file_int2_read = (file_int1_read + 1)
-                    file_to_read = file_read[file_int2_read]
+                    file_to_read = file_read[line_number]
                     file_to_read_stripped = file_to_read.strip()
-                    merit_total = int(file_to_read_stripped)
+                    output = file_to_read_stripped
 
-                    return merit_total
+                    return output
+
+login()
