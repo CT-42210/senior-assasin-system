4fc07df912885b1e13695a1d5ff8aed96bab039e
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-04-20 12:35:37 -0400

    basic-setup

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,11 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    session['username'] = "username"
+    session['username'] = "johannes"
+    # session.clear()
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
