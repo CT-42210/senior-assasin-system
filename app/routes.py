ba1e958bcd5de3490bbd35b9ae44c174daa253bf
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-04-20 19:16:28 -0400

    javascript-works login-basic-works

--- app/routes.py
+++ app/routes.py
@@ -2,23 +2,36 @@ from flask import render_template, request, session
 from app import app
 
 
+# / | /index
+#   home page of server
 @app.route('/')
 @app.route('/index')
 def index():
-    session['username'] = "johannes"
+    # session['username'] = "johannes"
     # session.clear()
     return render_template('index.html')
 
 
+# /login page
+#   if user already has an ccount they can log in here
 @app.route('/login')
 def login():
     return render_template("login.html")
 
 
+# /login_check
+#   checks if users are exist | are properly logging in, correct credentials
 @app.route("/login_check", methods=["POST"])
 def login_check():
 
-    username = request.form.get("text")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    session['username'] = username
 
     print(username)
+    print(password)
     return username
+
+
+# /signup
+#   sign-up page for a user
--- app/static/login_functions.js
+++ app/static/login_functions.js
@@ -1,18 +1,21 @@
 function sendData() {
-	if (document.getElementById("username").value.length == 0) {
-		console.log("No username!");
+	if (document.getElementById("username").value.length == 0 || document.getElementById("password").value.length == 0) {
+		console.log("ERROR! no password or username");
 		return;
 	} else {
 	      $.ajax({
         type: "POST",
         url: "/login_check",
-        data: { text: $("#username").val() },
+        data: { username: $("#username").val(),
+                password: $("#password").val() },
         success: function(response) {
         		console.log("submitting to python snake");
+        		console.log(response)
         }
       });
 		console.log("submitting to python");
 		document.getElementById("username").value = "";
+		document.getElementById("password").value = "";
 	}
 
 }
