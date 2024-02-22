a7ca60db914b3078f0fb51c663c3df4a88e79400
Nick T <nicholastroiano123@gmail.com>
2024-02-21 22:10:18 -0500

    some small updates

--- functions.py
+++ functions.py
@@ -110,7 +110,7 @@ def teammate_data(username):
                 print("ValueError")
 
 
-def Etotal_team_data():
+def total_team_data():
     return_list = []
 
     total_team_number = database.last_line_finder(team_names_file) + 1
